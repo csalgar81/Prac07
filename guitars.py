db290f5a8bdcedeb1d0d6c8d1884aaeeb2dc6d76
jc410053 <carlos.salgar1@my.jcu.edu.au>
2016-09-18 14:19:29 +1000

    c1

--- guitars.py
+++ guitars.py
@@ -1,15 +1,21 @@
 from guitar_class import Guitar
-guitars = []
-# name = input('Guitar name: ')
-# year = int(input('Year: '))
-# cost = float(input('Cost: $ '))
+# guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+# guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-# while name != '':
-    # name = input('Guitar name: ')
-    # year = int(input('Year: '))
-    # cost = float(input('Cost: $ '))
-    # guitar = Guitar(name, year, cost)
-    # guitars.append([guitar.name,guitar.year,guitar.cost])
-guitar = Guitar(input('Guitar name: '), int(input('Year: ')), 1000)
-# guitars.append([input('Guitar name: '),int(input('Year: '))] )
-    # print("{} ({}) : ${} added".format(name, year, cost))
+guitars = []
+
+guitar = Guitar(input('Name: '), int(input('Year: ')), float(input('Cost: $')))
+while guitar.name != '':
+    guitars.append(guitar)
+    print(guitar," added.")
+    guitar = Guitar(input('Guitar name: '), int(input('Year: ')), float(input('Cost: $')))
+
+print('These are my Guitars:')
+
+for i, guitar in enumerate(guitars):
+    if guitar.is_vintage():
+        vintage_string = '(vintage)'
+    else:
+        ''
+    print("Guitar {}: {:>20} ({}), worth ${:10.2f} {}".format(i + 1, guitar.name, guitar.year, guitar.cost,vintage_string))
+
